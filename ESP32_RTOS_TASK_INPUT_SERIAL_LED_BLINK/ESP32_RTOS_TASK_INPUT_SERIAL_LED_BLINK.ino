6e5d91bd55a646212cae6023f66548b9040253b2
RabbifromOffice <rabbi.iot@gmail.com>
2023-01-24 11:55:08 +0600

    commit from office

--- ESP32_RTOS_TASK_INPUT_SERIAL_LED_BLINK/ESP32_RTOS_TASK_INPUT_SERIAL_LED_BLINK.ino
+++ ESP32_RTOS_TASK_INPUT_SERIAL_LED_BLINK/ESP32_RTOS_TASK_INPUT_SERIAL_LED_BLINK.ino
@@ -1,6 +1,16 @@
 static const uint8_t length = 20;
 static const uint8_t led0 = 25;
 static int ledDelay = 500;
+void ledBlink(void *parameter)
+{
+    while (1)
+    {
+        digitalWrite(led0, HIGH);
+        vTaskDelay(ledDelay / portTICK_PERIOD_MS);
+        digitalWrite(led0, LOW);
+        vTaskDelay(ledDelay / portTICK_PERIOD_MS);
+    }
+}
 void setup()
 {
     Serial.begin(9600);
